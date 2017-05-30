639da438c948e0aad34dc3379c147134923caa9a
Daniel Huang <gn03249822@DanieltekiMacBook-Pro.local>
2017-05-29 19:10:18 -0700

    debugging the phi matrix

--- Viterbi.py
+++ Viterbi.py
@@ -4,24 +4,25 @@ import numpy as np
 """The function that computes the entries in the log-likelihood matrix"""
 def viterbiLog(logArr,transition, emission, observations):
      # base case where time t=1
-    for i in range(len(logArr)):
+    for i in xrange(len(logArr)):
         logArr[i][0]=np.log(pi[i])+np.log(emission[i][observations[0]]) #fill in the first column
 
 
-    for j in range(len(logArr)):
-        for t in range(len(logArr[j])):
-            if t!=0:
-                 logArr[j][t+1]=max([row[t] for row in logArr]+np.log([row[j] for row in transition]))+np.log(emission[j][observations[t+1]])
-
+    for j in xrange(len(logArr)):
+        for t in xrange(len(logArr[j])):
+            if t!=0 and t+1<len(logArr[j]):
+                 logArr[j][t+1]=np.max([row[t] for row in logArr]+np.log([row[j] for row in transition]))+np.log(emission[j][observations[t+1]])
 
     return logArr
 
 """The function that fill in the entries of the most likely transition array I"""
-def mostLikelytransition(I_arr,logArr,transition):
-    for j in range(len(I_arr)):
-        for t in range(len(I_arr[j])):
-            I_arr[j][t+1]=np.argmax(logArr)
-
+def mostLikelytransition(I_arr,logArr,logtransition):
+    for j in xrange(len(I_arr)):
+        for t in xrange(len(I_arr[j])) :
+            if t+1<len(I_arr[j]):
+                I_arr[j][t+1]=np.argmax([row[t] for row in logArr] + [row[j] for row in logtransition])
+                print(I_arr[j][t+1])
+    return I_arr
 
 
 
@@ -36,9 +37,28 @@ observations=np.genfromtxt("observations.txt")
 pi=np.genfromtxt("initialState.txt")
 
 row,col=27, 180000 #hard-coded the dimensions of the 2-D log array
-logArr=[[0 for x in range(col)] for y in range(row)]
+logArr=[[0 for x in xrange(col)] for y in xrange(row)]
 
 logArr=viterbiLog(logArr,transition,emission,observations)  #fill in every entry computed by Viterbi Algorithm
 
-I_arr=logArr=[[0 for x in range(col)] for y in range(row)] #I array has the same size as logArr
+I_arr=[[0 for x in xrange(col)] for y in xrange(row)] #I array has the same size as logArr
+logtransition=transition
+for entry in logtransition:
+    entry=np.log(entry)
 
+
+I_arr=mostLikelytransition(I_arr,logArr,logtransition)
+
+#Now we can compute the most likely sequence S*
+
+s_sequence=[0 for t in xrange(col)]
+
+s_sequence[col-1]=np.argmax([row[col-1] for row in logArr]) #at time=T
+print("S* at time T is : %d" %s_sequence[col-1])
+for t in xrange(col-2,-1,-1):
+    s_sequence[t]=I_arr[s_sequence[t+1]][t+1]
+
+#s_sequence=set(s_sequence)  #remove all repeated characters
+
+#for st in s_sequence:
+    #print(st)
